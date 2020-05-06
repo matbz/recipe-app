f280c41b8793949429a3b086bc18069e47bc2110
unknown <bentz.matthias@gmail.com>
2020-05-06 18:08:48 +0200

    update

--- client/config/dev.env.js
+++ client/config/dev.env.js
@@ -4,6 +4,8 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://localhost:5000"',
-  APP_URL: '"http://localhost:8080"',
+  APP_URL: '"http://192.168.2.114:8080"',
+  API_URL: '"http://192.168.2.114:5000"',
+  // APP_URL: '"http://localhost:8080"',
+  // API_URL: '"http://localhost:5000"',
 })
