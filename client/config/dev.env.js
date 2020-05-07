040abf719b7fe627a38cd99623b41e337ba4f042
matbz <bentz.matthias@gmail.com>
2020-05-07 20:37:36 +0200

    build

--- client/config/dev.env.js
+++ client/config/dev.env.js
@@ -6,6 +6,8 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // APP_URL: '"http://192.168.2.114:8080"',
   // API_URL: '"http://192.168.2.114:5000"',
+  // APP_URL: '"http://192.168.11.102:8080"',
+  // API_URL: '"http://192.168.11.102:5000"',
   APP_URL: '"http://localhost:8080"',
   API_URL: '"http://localhost:5000"',
 })
